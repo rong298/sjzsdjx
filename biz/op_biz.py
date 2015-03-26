f0dcc1fe929a41e4ffb647348caef825368080af
rongliang <rongliang@yunhu.net.cn>
2015-03-26 15:37:45 +0800

    重新调整错误定义

--- biz/op_biz.py
+++ biz/op_biz.py
@@ -8,7 +8,7 @@ import logging
 class OpBusiness(BaseBusiness):
 
     def normal_monitor(self, term=10):
-        sql = "select dama_platform,status,count(1) as count,avg(TIMESTAMPDIFF(SECOND, FROM_UNIXTIME(UNIX_TIMESTAMP(start_time)), FROM_UNIXTIME(UNIX_TIMESTAMP(end_time)))) as spendtime from pass_code_records where created>=DATE_SUB(NOW(),INTERVAL %s MINUTE)  group by dama_platform,status" % int(term)
+        sql = "select dama_platform,status,count(1) as count,avg(TIMESTAMPDIFF(SECOND, FROM_UNIXTIME(UNIX_TIMESTAMP(start_time)), FROM_UNIXTIME(UNIX_TIMESTAMP(end_time)))) as spendtime from pass_code_records where created>=DATE_SUB(NOW(),INTERVAL %s MINUTE) and status!=0 group by dama_platform,status" % int(term)
         res = self.db.query(sql)
         logging.debug('[normal_monitor]%s', sql)
 
