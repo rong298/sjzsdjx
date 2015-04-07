dc851c3f79133fa4fadd1541889258d2b794f060
rongliang <rongliang@yunhu.net.cn>
2015-04-07 18:16:56 +0800

    余额统计

--- php_server/dama/balance.php
+++ php_server/dama/balance.php
@@ -8,11 +8,6 @@ ini_set("error_log", "/tmp/error_log.log");
 require 'dama2Lib/Dama2CurlApi.php';
 $dama_api = new Dama2Api('yunhu001', 'yur140608');
 
-
-
-var_dump(11124);
 $result = $dama_api->get_balance();
-var_dump(11123);
 echo json_encode($result);
-
 ?>
